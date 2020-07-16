54397f0b280e3167729eb9ca5a6639269d206e5c
will <545512533@qq.com>
2020-07-17 06:49:14 +0800

    添加汇编

---
+++ compilation/02.asm
@@ -0,0 +1,168 @@
+﻿assume cs:code
+OriginalStr segment
+    db 'Hello world!'
+OriginalStr ends
+
+EncryStr segment    
+    db 12 dup (00h)
+EncryStr ends
+
+DecryStr segment
+    db 12 dup (00h)
+DecryStr ends
+
+words segment
+    db "The original string is:"
+	db "The encryption string :"
+	db "The decryption string :"
+words ends
+
+stack segment
+    db 12 dup (00h)
+stack ends
+
+code segment
+start:
+	;加密过程
+	mov ax,stack
+	mov ss,ax
+	mov sp,16
+	mov ax,OriginalStr
+	mov ds,ax
+	mov bx,0
+	mov cx,11
+	mov dl,2
+s1:
+	mov al,[bx]
+	add al,[bx+1]
+	mov ah,0
+	div dl
+	mov [bx+10h],al
+	inc bx
+	push ax
+	loop s1
+	mov al,[bx]
+	add al,20h
+	mov ah,0
+	div dl
+	mov [bx+10h],al
+	push ax
+
+	;解密过程
+	mov ax,EncryStr
+	mov ds,ax
+	mov bx,11
+	mov cx,12
+	mov dl,2
+	pop ax
+	mov al,ah
+	add al,[bx]
+	add al,[bx]
+	sub al,20h
+	mov [bx+10h],al
+	dec bx
+s2:
+	pop ax
+	mov al,ah
+	add al,[bx]
+	add al,[bx]
+	sub al,[bx+11h]
+	mov [bx+10h],al
+	dec bx
+	loop s2
+
+	;屏幕显示
+	mov ax,0b878h
+	mov ds,ax
+	mov ax,words
+	mov es,ax
+	mov bx,0
+	mov ah,1110010b;
+	mov cx,23
+s3:
+	mov al,es:[bx]
+	mov si,bx
+	add si,bx
+	mov [si],al
+	mov [si+1],ah
+	inc bx
+	loop s3
+	mov ax,OriginalStr
+	mov es,ax
+	mov cx,12
+	mov bx,0
+	mov ah,1110010b;
+s6:
+	mov al,es:[bx]
+	mov si,46
+	add si,bx 
+	add si,bx
+	mov [si],al
+	mov [si+1],ah
+	inc bx
+	loop s6
+
+	mov ax,0b88ch
+	mov ds,ax
+	mov ax,words
+	mov es,ax
+	mov bx,0
+	mov ah,1110100b;
+	mov cx,23
+s4:
+	mov al,es:[bx]
+	mov si,bx
+	add si,bx
+	mov [si],al
+	mov [si+1],ah
+	inc bx
+	loop s4
+	mov ax,EncryStr
+	mov es,ax
+	mov cx,12
+	mov bx,0
+	mov ah,1110100b;
+s7:
+	mov al,es:[bx]
+	mov si,46
+	add si,bx 
+	add si,bx
+	mov [si],al
+	mov [si+1],ah
+	inc bx
+	loop s7
+
+	mov ax,0b8a0h
+	mov ds,ax
+	mov ax,words
+	mov es,ax
+	mov bx,0
+	mov ah,1110001b;
+	mov cx,23
+s5:
+	mov al,es:[bx]
+	mov si,bx
+	add si,bx
+	mov [si],al
+	mov [si+1],ah
+	inc bx
+	loop s5
+	mov ax,DecryStr
+	mov es,ax
+	mov cx,12
+	mov bx,0
+	mov ah,1110001b;
+s8:
+	mov al,es:[bx]
+	mov si,46
+	add si,bx 
+	add si,bx
+	mov [si],al
+	mov [si+1],ah
+	inc bx
+	loop s8
+
+	mov ax,4c00h
+	int 21h
+code ends
+end start
---
+++ compilation/03.asm
@@ -0,0 +1,115 @@
+﻿assume cs:code
+
+Calculate segment
+    db '1.3/1=          '
+	db '2.5+3=          '
+    db '3.9-3=          '
+    db '4.4+5=          '
+Calculate ends
+
+stack segment
+	db 16 dup (0)
+stack ends
+
+code segment
+s1:
+	mov cl,[bx+3]
+	sub cl,'-'
+	add cl,1
+	loop s2
+
+	;减法
+	mov dl,[bx+4]
+	sub dl,30h
+	mov al,[bx]
+	sub al,30h
+	sub al,dl
+	add al,30h
+	mov [bx+6],al
+	mov byte ptr [bx+7],'.'
+	mov al,[bx+2]
+	mov [bx+8],al
+	add bx,10h
+	pop cx
+	loop s
+	mov cx,1
+	loop s0
+
+s2:
+	;除法
+	mov dl,[bx+4]
+	sub dl,30h
+	mov al,[bx]
+	sub al,30h
+	div dl
+	add al,30h
+	mov [bx+6],al
+	mov byte ptr [bx+7],'.'
+	mov ah,0
+	mov al,[bx+2]
+	sub al,30h
+	div dl
+	add al,30h
+	mov [bx+8],al
+	add bx,10h
+	pop cx
+	loop s
+	mov cx,1
+	loop s0
+
+	;程序入口
+start:
+	mov ax,stack
+	mov ss,ax
+	mov sp,16
+	mov cx,4
+	mov bx,0
+	mov ax,Calculate
+	mov ds,ax
+	mov ah,0
+	s:
+	push cx
+	mov cl,[bx+3]
+	sub cl,'+'
+	add cl,1
+	loop s1
+
+	;加法
+	mov dl,[bx+4]
+	sub dl,30h
+	mov al,[bx]
+	sub al,30h
+	add al,dl
+	add al,30h
+	mov [bx+6],al
+	mov byte ptr [bx+7],'.'
+	mov al,[bx+2]
+	mov [bx+8],al
+	add bx,10h
+	pop cx
+	loop s
+	mov cx,1
+	loop s0
+
+	;打印结果
+s0:
+	mov ax,Calculate 
+	mov es,ax
+	mov bx,0
+	mov ax,0b878h
+	mov ds,ax
+	mov ah,10111b
+	mov cx,64
+s3:
+	mov al,es:[bx]
+	mov si,bx
+	add si,bx
+	mov [si],al
+	mov [si+1],ah
+	inc bx
+	loop s3
+
+	mov ax,4c00h
+	int 21h
+code ends
+end start
